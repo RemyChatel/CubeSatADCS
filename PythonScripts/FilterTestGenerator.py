a60a8ab49a7cc0319ca8356386f630df12c328f2
Remy CHATEL <chatel.remy@gmail.com>
2019-08-05 12:55:12 +0100

    Improving Python Scripts

--- PythonScripts/FilterTestGenerator.py
+++ PythonScripts/FilterTestGenerator.py
@@ -1,9 +1,9 @@
-import numpy as np;
-import matplotlib.pyplot as pl;
+import numpy as np
+import matplotlib.pyplot as pl
 import scipy.integrate as int
 
 def quat_mul(leftQ, rightQ):
-	res = np.zeros((4,1));
+	res = np.zeros((4,1))
 	res[0] = leftQ[0]*rightQ[0] - leftQ[1]*rightQ[1] - leftQ[2]*rightQ[2] - leftQ[3]*rightQ[3]
 	res[1] = leftQ[1]*rightQ[0] + leftQ[0]*rightQ[1] + leftQ[2]*rightQ[3] - leftQ[3]*rightQ[2]
 	res[2] = leftQ[0]*rightQ[2] - leftQ[1]*rightQ[3] + leftQ[2]*rightQ[0] + leftQ[3]*rightQ[1]
@@ -24,32 +24,32 @@ def quat_exp(q):
 
 ################################################################################
 # Simulation parameters
-t_end = 10;
-N = 1000;
+t_end = 10
+N = 1000
 # Init quat_th0# Generate omega_th(k)
 # ZYX 60->-45->30	    eta     x       y      z
-# quat_th0  = np.array([0.0723,0.392,-0.2010, 0.5320], dtype="float");
-quat_th0  = np.array([0,1,0,0], dtype="float");
-omega_th0 = np.array([90 * np.pi/180, 1 * np.pi/180, 0 * np.pi/180], dtype="float");
+# quat_th0  = np.array([0.0723,0.392,-0.2010, 0.5320], dtype="float")
+quat_th0  = np.array([1,0,0,0], dtype="float")
+omega_th0 = np.array([90 * np.pi/180, 45 * np.pi/180, 0 * np.pi/180], dtype="float")
 # Kalman P initial covaraince matrix setup
-sigma_p = 0.1;
+sigma_p = 0.01
 # Kalman Q process noise matrix setup
-sigma_q = 0.01;
+sigma_q = 0.01
 # Kalman R measurement noise matrix setup
-sigma_eta = 0.05;
-sigma_epsilon = 0.05;
-sigma_omega = 0.1;
+sigma_eta = 0.05
+sigma_epsilon = 0.05
+sigma_omega = 0.05
 
 I = np.diag([27,17,25])
 I_inv = np.linalg.inv(I)
 
 
-t = np.linspace(0,t_end, N);
-delta_t = np.zeros(t.size);
-omega_th = np.zeros((4,t.size));
-quat_th = np.zeros((4,t.size));
-state = np.zeros((7,t.size));
-state0 = np.zeros(7);
+t = np.linspace(0,t_end, N)
+delta_t = np.zeros(t.size)
+omega_th = np.zeros((4,t.size))
+quat_th = np.zeros((4,t.size))
+state = np.zeros((7,t.size))
+state0 = np.zeros(7)
 state0[:4] = quat_th0
 state0[4:] = omega_th0
 
@@ -76,12 +76,12 @@ for i in range(t.size):
 ################################################################################
 # Add noise and pack to state vec
 
-quat_noise = np.zeros((4,t.size), dtype="float");
-omega_noise = np.zeros((4,t.size), dtype="float");
+quat_noise = np.zeros((4,t.size), dtype="float")
+omega_noise = np.zeros((4,t.size), dtype="float")
 
-quat_noise[0,:] = quat_th[0,:] + np.random.normal(0, sigma_eta, (t.size));
-quat_noise[1:,:] = quat_th[1:,:] + np.random.normal(0, sigma_epsilon, (3,t.size));
-omega_noise[1:,:] = omega_th[1:,:] + np.random.normal(0, sigma_omega, (3,t.size));
+quat_noise[0,:] = quat_th[0,:] + np.random.normal(0, sigma_eta, (t.size))
+quat_noise[1:,:] = quat_th[1:,:] + np.random.normal(0, sigma_epsilon, (3,t.size))
+omega_noise[1:,:] = omega_th[1:,:] + np.random.normal(0, sigma_omega, (3,t.size))
 quat_pred = np.copy(quat_noise)
 omega_pred = np.copy(omega_noise)
 
@@ -98,8 +98,8 @@ string += 'sigma_omega = {:f};\n'.format(sigma_omega)
 string += 'size = {:d};\n'.format(N)
 string += 'delta = {:f};\n'.format(t_end/N)
 string += 'float quat[{:d}] = {{\n'.format(4*t.size)
-np.savetxt('../ADCS/Tests/quat.data', np.transpose(quat_export), fmt='%+f', delimiter=',', newline=',\n', header=string, footer='};', comments='')
-np.savetxt('../ADCS/Tests/omega.data', np.transpose(omega_export), fmt='%+f', delimiter=',', newline=',\n', header='float omega[{:d}] = {{\n'.format(3*t.size), footer='};', comments='')
+np.savetxt('../ADCS/Filters/quat.data',np.transpose(quat_export), fmt='%+f', delimiter=',', newline=',\n', header=string, footer='};', comments='')
+np.savetxt('../ADCS/Filters/omega.data',np.transpose(omega_export), fmt='%+f', delimiter=',', newline=',\n', header='float omega[{:d}] = {{\n'.format(3*t.size), footer='};', comments='')
 
 # Saving for the plotting
 np.savetxt('t.csv', t, delimiter=',')
---
+++ PythonScripts/FilterTestPlotter.py
@@ -0,0 +1,176 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+def quat_norm(q):
+	return np.sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3])
+
+def quat_conj(q):
+	res = np.zeros((4,1))
+	res[0] =  q[0]
+	res[1] = -q[1]
+	res[2] = -q[2]
+	res[3] = -q[3]
+	return res
+
+def quat_inv(q):
+	res = np.zeros((4,1))
+	res = quat_conj(q)/quat_norm(q)
+	return res
+
+def quat_mul(leftQ, rightQ):
+	res = np.zeros((4,1))
+	res[0] = leftQ[0]*rightQ[0] - leftQ[1]*rightQ[1] - leftQ[2]*rightQ[2] - leftQ[3]*rightQ[3]
+	res[1] = leftQ[1]*rightQ[0] + leftQ[0]*rightQ[1] + leftQ[2]*rightQ[3] - leftQ[3]*rightQ[2]
+	res[2] = leftQ[0]*rightQ[2] - leftQ[1]*rightQ[3] + leftQ[2]*rightQ[0] + leftQ[3]*rightQ[1]
+	res[3] = leftQ[0]*rightQ[3] + leftQ[1]*rightQ[2] - leftQ[2]*rightQ[1] + leftQ[3]*rightQ[0]
+	return res
+
+def quat2euler(q):
+	test = q[1]*q[2] + q[3]*q[0]
+	if (test > 0.499):
+		heading = 2 * np.arctan2(q[1],q[0])
+		attitude = np.pi/2
+		bank = 0
+		return
+
+	if (test < -0.499):
+		heading = -2 * np.arctan2(q[1],q[0])
+		attitude = - np.pi/2
+		bank = 0
+		return
+	
+	sqx = q[1]*q[1]
+	sqy = q[2]*q[2]
+	sqz = q[3]*q[3]
+	heading = np.arctan2(2*q[2]*q[0]-2*q[1]*q[3] , 1 - 2*sqy - 2*sqz)
+	attitude = np.arcsin(2*test)
+	bank = np.arctan2(2*q[1]*q[0]-2*q[2]*q[3] , 1 - 2*sqx - 2*sqz)
+	euler = np.zeros(3)
+	euler[0] = bank
+	euler[1] = attitude
+	euler[2] = heading
+	return euler
+
+t			= np.loadtxt('t.csv', delimiter=',')
+quat_th		= np.loadtxt('quaternion_th.csv', delimiter=',')
+omega_th 	= np.loadtxt('omega_th.csv', delimiter=',')
+quat_no		= np.loadtxt('quaternion_noise.csv', delimiter=',')
+omega_no	= np.loadtxt('omega_noise.csv', delimiter=',')
+
+quat_pred 	= np.loadtxt('quaternion_pred.csv', delimiter=',')
+omega_pred 	= np.loadtxt('omega_pred.csv', delimiter=',')
+delta_t		= np.loadtxt('delta_t.csv', delimiter=',')
+
+filtered_error = np.zeros((3,t.size))
+initial_error = np.zeros((3,t.size))
+for i in range(t.size):
+	filtered_error[:,i] = quat2euler( quat_mul( quat_th[:,i], quat_inv(quat_pred[:,i]) ) )
+for i in range(t.size):
+	initial_error[:,i] = quat2euler( quat_mul( quat_th[:,i], quat_inv(quat_no[:,i]) ) )
+
+# plot quat_predict and omega_predict
+pl.subplots(2,4, figsize=(10,7))
+pl.suptitle("Quaternion")
+pl.subplot(2,2,1)
+pl.plot(t, quat_no[0,:], label='Measured', color='grey')
+pl.plot(t, quat_th[0,:], label='Noisy estimation')
+pl.plot(t, quat_pred[0,:], label='Filtered prediction', linestyle='dashed')
+pl.legend()
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\eta$")
+pl.grid(True)
+pl.ylim(-1.1,1.1)
+pl.legend()
+pl.subplot(2,2,2)
+pl.plot(t, quat_no[1,:], label='Measured', color='grey')
+pl.plot(t, quat_th[1,:], label='Noisy estimation')
+pl.plot(t, quat_pred[1,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\epsilon_x$")
+pl.grid(True)
+pl.ylim(-1.1,1.1)
+pl.legend()
+pl.subplot(2,2,3)
+pl.plot(t, quat_no[2,:], label='Measured', color='grey')
+pl.plot(t, quat_th[2,:], label='Noisy estimation')
+pl.plot(t, quat_pred[2,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\epsilon_y$")
+pl.grid(True)
+pl.ylim(-1.1,1.1)
+pl.legend()
+pl.subplot(2,2,4)
+pl.plot(t, quat_no[3,:], label='Measured', color='grey')
+pl.plot(t, quat_th[3,:], label='Noisy estimation')
+pl.plot(t, quat_pred[3,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\epsilon_z$")
+pl.grid(True)
+pl.ylim(-1.1,1.1)
+pl.legend()
+
+pl.subplots(3,1, figsize=(10,7))
+pl.suptitle('Angular error between theoretical input and actual output')
+pl.subplot(3,1,1)
+pl.plot(t, initial_error[0]*180/np.pi, label="Input error", linestyle='dashed')
+pl.plot(t, filtered_error[0]*180/np.pi, label="Output error")
+pl.xlabel("Time (s)")
+pl.ylabel("Roll anglular error (deg)")
+pl.ylim(-10,10)
+pl.grid(True)
+pl.legend()
+pl.subplot(3,1,2)
+pl.plot(t, initial_error[1]*180/np.pi, label="Input error", linestyle='dashed')
+pl.plot(t, filtered_error[1]*180/np.pi, label="Output error")
+pl.xlabel("Time (s)")
+pl.ylabel("Pitch anglular error (deg)")
+pl.ylim(-10,10)
+pl.grid(True)
+pl.legend()
+pl.subplot(3,1,3)
+pl.plot(t, initial_error[2]*180/np.pi, label="Input error", linestyle='dashed')
+pl.plot(t, filtered_error[2]*180/np.pi, label="Output error")
+pl.xlabel("Time (s)")
+pl.ylabel("Yaw anglular error (deg)")
+pl.ylim(-10,10)
+pl.grid(True)
+pl.legend()
+
+pl.subplots(2,4, figsize=(10,7))
+pl.suptitle("Angular rate and timing")
+pl.subplot(2,2,1)
+# pl.plot(t, omega_no[1,:], label='Measured', linestyle='dashed', color='grey')
+pl.plot(t, omega_th[1,:], label='Theoretical rate')
+pl.plot(t, omega_pred[1,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\omega_x$")
+pl.grid(True)
+pl.legend()
+pl.ylim(-2*np.pi, np.pi)
+pl.subplot(2,2,2)
+# pl.plot(t, omega_no[2,:], label='Measured', color='grey')
+pl.plot(t, omega_th[2,:], label='Theoretical rate')
+pl.plot(t, omega_pred[2,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\omega_y$")
+pl.grid(True)
+pl.legend()
+pl.ylim(-2*np.pi, np.pi)
+pl.subplot(2,2,3)
+# pl.plot(t, omega_no[3,:], label='Measured', linestyle='dashed', color='grey')
+pl.plot(t, omega_th[3,:], label='Theoretical rate')
+pl.plot(t, omega_pred[3,:], label='Filtered prediction', linestyle='dashed')
+pl.xlabel("Time (s)")
+pl.ylabel(r"$\omega_z$")
+pl.grid(True)
+pl.legend()
+pl.ylim(-2*np.pi, np.pi)
+pl.subplot(2,2,4)
+pl.plot(t, delta_t/1000, label='Average {:f} ms'.format(np.average(delta_t)/1000))
+pl.ylim(0,6)
+pl.xlabel("Time (s)")
+pl.ylabel("Timing (ms)")
+pl.grid(True)
+pl.legend()
+
+pl.show()
